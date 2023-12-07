040ddde716235805118c33c4152883a67b8eb361
SamratCodess <samratcodess@gmail.com>
2023-12-07 18:40:39 +0545

    initialize

--- Ecommerce/src/app/app.module.ts
+++ Ecommerce/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
